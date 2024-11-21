152a65e8821fd8f890aac6e948315ba33d9e21bf
salitayu <86502199+salitayu@users.noreply.github.com>
2024-11-20 23:19:29 -0500

    shows table material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,89 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import Paper from '@mui/material/Paper'
+import React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+
+interface Data {
+  id: number;
+  showName: string;
+  showTime: Date;
+  rating: number;
+}
+
+function createData(
+  id: number,
+  showName: string,
+  showTime: Date,
+  rating: number
+): Data {
+  return {
+    id,
+    showName,
+    showTime,
+    rating
+  }
+}
+
+const rows = [
+  createData(1, 'Recreo', new Date("2024-11-20"), 8),
+  createData(2, 'Brandy y el senor bigotes', new Date("2024-11-15"), 9),
+  createData(3, 'Hey Arnold', new Date("2024-11-18"), 7),
+  createData(4, 'Las Pistas de Blue', new Date("2024-11-16"), 6),
+  createData(5, 'Bob Esponja', new Date("2024-11-12"), 10),
+  createData(6, 'Los padrinos magicos', new Date("2024-11-17"), 8),
+  createData(8, 'Jimmy Neutron', new Date("2024-11-18"), 10),
+  createData(9, 'Drake y Josh',  new Date("2024-11-19"), 6),
+  createData(10, 'Lizzy Mcguire',  new Date("2024-11-02"), 5),
+  createData(11, 'Wix',  new Date("2024-11-03"), 6),
+  createData(12, 'Chicas Superpoderosas', new Date("2024-11-05"), 9),
+  createData(13, 'Picapiedras',  new Date("2024-11-02"), 8),
+]
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]){
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]){
+    return 1
+  }
+  return 0
+}
+
+type Order = 'asc' | 'desc'
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'showName', headerName: 'Show Name', width: 130},
+  { field: 'showTime', headerName: 'Show Time', width: 450},
+  { field: 'rating', headerName: 'Rating', width: 70}
+]
+
+const paginationModel = { page: 0, pageSize: 5 }
+
+export default function DataTable() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Paper sx={{height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{pagination: {paginationModel}}}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+      />
+    </Paper>
   )
 }
